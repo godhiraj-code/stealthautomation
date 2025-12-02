ea3a243e8ab242bceccb2a5fe117ae7796f29fa2
dhiraj <dhirajdas.666@gmail.com>
2025-12-02 18:42:54 +0530

    updated the setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sb-stealth-wrapper",
-    version="0.1.0",
+    version="0.1.1",
     description="A robust, plug-and-play wrapper around SeleniumBase UC Mode for stealth web automation.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
